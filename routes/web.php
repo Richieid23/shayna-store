4335cf23e4e65cb89d20e2919dd84d65a5ef5365
Richieid23 <fitrarizki23@gmail.com>
2021-07-23 06:58:57 +0700

    add details page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('/details/{id}', [DetailController::class, 'index'])->name('details');
 
 Auth::routes();
