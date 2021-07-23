0fad1a4a3c225b045dcc8cd919bec57ce6e1d21a
Richieid23 <fitrarizki23@gmail.com>
2021-07-24 05:47:00 +0700

    add transactions page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardTransactionController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
@@ -33,5 +34,7 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-products');
 Route::get('/dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-products-create');
 Route::get('/dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-products-details');
+
+Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'index'])->name('dashboard-transactions');
 
 Auth::routes();
