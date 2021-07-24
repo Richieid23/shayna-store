f9fdc027aa0a377f2c2881f2a3ec1f93759f3b8e
Richieid23 <fitrarizki23@gmail.com>
2021-07-24 13:24:33 +0700

    add dashboard statistics & datatables lib

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -14,6 +16,13 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.admin.dashboard');
+        $customer = User::count();
+        $revenue = Transaction::sum('total_price');
+        $transaction = Transaction::count();
+        return view('pages.admin.dashboard', [
+            'customer' => $customer,
+            'revenue' => $revenue,
+            'transaction' => $transaction,
+        ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -25,7 +25,7 @@
                           Customer
                         </div>
                         <div class="dashboard-card-subtitle">
-                          15,209
+                          {{ $customer }}
                         </div>
                       </div>
                     </div>
@@ -37,7 +37,7 @@
                           Revenue
                         </div>
                         <div class="dashboard-card-subtitle">
-                          $931,290
+                          ${{ $revenue }}
                         </div>
                       </div>
                     </div>
@@ -49,107 +49,13 @@
                           Transaction
                         </div>
                         <div class="dashboard-card-subtitle">
-                          22,409,399
+                          {{ $transaction }}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="row mt-3">
-                  <div class="col-12 mt-2">
-                    <h5 class="mb-3">Recent Transactions</h5>
-                    <a
-                      class="card card-list d-block"
-                      href="/dashboard-transactions-details.html"
-                    >
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-md-1">
-                            <img
-                              src="/images/dashboard-icon-product-1.png"
-                              alt=""
-                            />
-                          </div>
-                          <div class="col-md-4">
-                            Shirup Marzzan
-                          </div>
-                          <div class="col-md-3">
-                            Angga Risky
-                          </div>
-                          <div class="col-md-3">
-                            12 Januari, 2020
-                          </div>
-                          <div class="col-md-1 d-none d-md-block">
-                            <img
-                              src="/images/dashboard-arrow-right.svg"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                    <a
-                      class="card card-list d-block"
-                      href="/dashboard-transactions-details.html"
-                    >
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-md-1">
-                            <img
-                              src="/images/dashboard-icon-product-2.png"
-                              alt=""
-                            />
-                          </div>
-                          <div class="col-md-4">
-                            LeBrone X
-                          </div>
-                          <div class="col-md-3">
-                            Masayoshi
-                          </div>
-                          <div class="col-md-3">
-                            11 January, 2020
-                          </div>
-                          <div class="col-md-1 d-none d-md-block">
-                            <img
-                              src="/images/dashboard-arrow-right.svg"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                    <a
-                      class="card card-list d-block"
-                      href="/dashboard-transactions-details.html"
-                    >
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-md-1">
-                            <img
-                              src="/images/dashboard-icon-product-3.png"
-                              alt=""
-                            />
-                          </div>
-                          <div class="col-md-4">
-                            Soffa Lembutte
-                          </div>
-                          <div class="col-md-3">
-                            Shayna
-                          </div>
-                          <div class="col-md-3">
-                            11 January, 2020
-                          </div>
-                          <div class="col-md-1 d-none d-md-block">
-                            <img
-                              src="/images/dashboard-arrow-right.svg"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
+
               </div>
             </div>
           </div>
