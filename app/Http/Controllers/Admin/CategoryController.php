e770f080d5e1131d076f6dfb8a26140b58061413
Richieid23 <fitrarizki23@gmail.com>
2021-07-25 09:48:25 +0700

    add category update & delete

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -101,7 +101,9 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = Category::findOrFail($id);
+
+        return view('pages.admin.category.edit', ['item' => $item]);
     }
 
     /**
@@ -113,7 +115,16 @@ class CategoryController extends Controller
      */
     public function update(CategoryRequest $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->name);
+        $data['photo'] = $request->file('photo')->store('assets/category', 'public');
+
+        $item = Category::findOrFail($id);
+
+        $item->update($data);
+
+        return redirect()->route('category.index');
     }
 
     /**
@@ -124,6 +135,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Category::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('category.index');
     }
 }
