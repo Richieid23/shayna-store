cb5bac0fbec971d25241290d46958b5b118230af
Richieid23 <fitrarizki23@gmail.com>
2021-07-26 05:36:17 +0700

    fixing layouts

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
-      <div class="container">
+    <div class="container">
         <div class="row">
-          <div class="col-12 text-center">
-            <p class="pt-4 pb-2">
-              2019 Copyright Store. All Rights Reserved.
-            </p>
-          </div>
+            <div class="col-12 text-center">
+                <p class="pt-4 pb-2">
+                    2019 Copyright Store. All Rights Reserved.
+                </p>
+            </div>
         </div>
-      </div>
-    </footer>
+    </div>
+</footer>
