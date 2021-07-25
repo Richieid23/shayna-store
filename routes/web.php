2034586c730c2ffaa2ca002e98a0d770676ed3b2
Richieid23 <fitrarizki23@gmail.com>
2021-07-25 14:12:28 +0700

    add landing page integration categories section

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('/categories/{id}', [CategoryController::class, 'detail'])->name('categories-detail');
 Route::get('/details/{id}', [DetailController::class, 'index'])->name('details');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/success', [CartController::class, 'success'])->name('success');
