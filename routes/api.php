ed015a3155dea02c9d20ff1567a062ffb80cd5d9
Richieid23 <fitrarizki23@gmail.com>
2021-07-26 21:09:01 +0700

    add location api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LocationController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('register/check', [RegisterController::class, 'check'])->name('api-register-check');
+Route::get('provinces', [LocationController::class, 'provinces'])->name('api-provinces');
+Route::get('regencies/{provinces_id}', [LocationController::class, 'regencies'])->name('api-regencies');
