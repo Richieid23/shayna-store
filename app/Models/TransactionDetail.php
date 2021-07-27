cbc9de1d46b0467773fe8a4d993869122c72a99d
Richieid23 <fitrarizki23@gmail.com>
2021-07-27 09:51:55 +0700

    add transaction detail model

---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'transactions_id',
+        'product_id',
+        'price',
+        'shipping_status',
+        'resi',
+        'code'
+    ];
+
+    protected $hidden = [];
+}
