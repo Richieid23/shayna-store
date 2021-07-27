2d6542fb84fa55ed5453766bbf52879d900aa0d2
Richieid23 <fitrarizki23@gmail.com>
2021-07-27 09:54:37 +0700

    fixing user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,14 @@ class User extends Authenticatable
         'roles',
         'store_name',
         'categories_id',
-        'store_status'
+        'store_status',
+        'address_one',
+        'address_two',
+        'provinces_id',
+        'regencies_id',
+        'zip_code',
+        'country',
+        'phone_number',
     ];
 
     /**
