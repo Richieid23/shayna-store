a76fa14440c22ae53b040f1b7fb48d341c8205bb
Richieid23 <fitrarizki23@gmail.com>
2021-07-27 18:25:37 +0700

    add dashboard controller integration

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransactionDetail;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -13,6 +16,21 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard');
+        $transaction = TransactionDetail::with(['transaction.user', 'product.galleries'])->whereHas('product', function ($product) {
+            $product->where('users_id', Auth::user()->id);
+        });
+
+        $revenue = $transaction->get()->reduce(function ($carry, $item) {
+            return $carry + $item->price;
+        });
+
+        $customer = User::count();
+
+        return view('pages.dashboard', [
+            'transaction_count' => $transaction->count(),
+            'transaction_data' => $transaction->get(),
+            'revenue' => $revenue,
+            'customer' => $customer
+        ]);
     }
 }
