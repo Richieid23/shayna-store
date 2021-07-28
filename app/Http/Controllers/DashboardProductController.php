ff3e4085991565d8863b0c699ae2fb49b6d16b0b
Richieid23 <fitrarizki23@gmail.com>
2021-07-28 07:07:55 +0700

    add product store integration

--- app/Http/Controllers/DashboardProductController.php
+++ app/Http/Controllers/DashboardProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Admin\ProductRequest;
+use App\Models\ProductGallery;
 
 class DashboardProductController extends Controller
 {
@@ -29,5 +32,23 @@ class DashboardProductController extends Controller
     {
         $categories = Category::all();
         return view('pages.dashboard-products-create', ['categories' => $categories]);
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->name);
+
+        $product = Product::create($data);
+
+        $gallery = [
+            'products_id' => $product->id,
+            'photos' => $request->file('photo')->store('assets/product', 'public')
+        ];
+
+        ProductGallery::create($gallery);
+
+        return redirect()->route('dashboard-products');
     }
 }
